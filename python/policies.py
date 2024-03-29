5995986599b92fad412f7ba39d87e10ac02fc3ad
Justin Laws <86179207+sampleworthy@users.noreply.github.com>
2024-03-29 15:31:19 -0400

    Update policies.py

--- python/policies.py
+++ python/policies.py
@@ -10,13 +10,14 @@ product_id = os.getenv('product_id')
 # Path to the policies.xml file
 policy_file_path = './policies/policies.xml'
 
-# Construct the command to create the product policy in APIM
+# Construct the command to update the product policy in APIM
 command = [
-    'az', 'apim', 'product', 'policy', 'create',
+    'az', 'apim', 'product', 'policy', 'update',
     '--resource-group', resourceGroupName,
     '--service-name', apimServiceName,
     '--product-id', product_id,
     '--xml-policy', policy_file_path,
+    '--set', 'format=xml',
 ]
 
 # Run the command
